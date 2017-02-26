8f1e18b2fbbea725c441d4816dff4905c8eb1a35
John Trapp <johneyt54@gmail.com>
2017-02-26 16:18:57 -0700

    It finally Works

--- Homework2/main.py
+++ Homework2/main.py
@@ -8,10 +8,19 @@ while (True):
     status, frame = cap.read()
 
     # Our operations on the frame come here
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # define range of blue color in HSV
+    lower_blue = np.array([110, 50, 50])
+    upper_blue = np.array([130, 255, 255])
+
+    # Threshold the HSV image to get only blue colors
+    mask = cv.inRange(hsv, lower_blue, upper_blue)
 
     # Display the resulting frame
-    cv.imshow('frame', frame)
+    cv.imshow('Lab 2', frame)
+    cv.imshow('HSV View', hsv)
+    cv.imshow('Object Tracking', mask)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
