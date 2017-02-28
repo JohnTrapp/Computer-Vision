f3db8fc189ad121644c918e9a713c1cb6e5de08c
John Trapp <johneyt54@gmail.com>
2017-02-27 23:28:14 -0700

    PYHTON!!!!
    grrrrrr

--- Homework2/main.py
+++ Homework2/main.py
@@ -3,28 +3,40 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-while (True):
-    # Capture frame-by-frame
-    status, frame = cap.read()
 
-    # Our operations on the frame come here
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+def main():
+    def onMouse( event, x, y, f, void ):
+        pix = event.at<f>(y, x)
+        b = pix.val[0]
+        g = pix.val[1]
+        r = pix.val[2]
+        pass
 
-    # define range of blue color in HSV
-    lower_blue = np.array([110, 50, 50])
-    upper_blue = np.array([130, 255, 255])
+    while (True):
+        # Capture frame-by-frame
+        status, frame = cap.read()
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
+        # Our operations on the frame come here
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    # Display the resulting frame
-    cv.imshow('Lab 2', frame)
-    cv.imshow('HSV View', hsv)
-    cv.imshow('Object Tracking', mask)
+        # define range of blue color in HSV
+        lower_blue = np.array([110, 50, 50])
+        upper_blue = np.array([130, 255, 255])
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Threshold the HSV image to get only blue colors
+        mask = cv.inRange(hsv, lower_blue, upper_blue)
 
+        # Display the resulting frame
+        cv.imshow('Lab 2', frame)
+        cv.imshow('HSV View', hsv)
+        cv.imshow('Object Tracking', mask)
+
+        cv.setMouseCallback('HSV View', onMouse, 0)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+main()
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
